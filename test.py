58409eb035988134327b427f5cc4fbdeddd3de40
wangzi <wangzi@git.imagedt.com>
2017-10-17 18:41:02 +0800

    easier to change # layers

--- test.py
+++ test.py
@@ -4,8 +4,7 @@ a = np.array(([1, 0, -1], [2, 3, 0]))
 b = np.zeros(a.shape)
 b[a > 0] = a[a > 0]
 c = np.array(a)
-print c
-c[0, 0] = -2
-print a * c
-print a
-print b, b.shape[0]
+d = [0, 3]
+d.append(a)
+for i in range(len(d))[::-1]:
+    print d[i]
